6fade3089f446021c0026487202040f49093db98
moodHappy <lhwuhuei@gmail.com>
2023-12-22 02:35:02 +0800

    Update Change Keyword Color.js

--- Change Keyword Color.js
+++ Change Keyword Color.js
@@ -3,6 +3,12 @@
 // @namespace    https://greasyfork.org/
 // @version      1.0
 // @description  Change the color of keywords on web pages according to their categories
+   @match        *://*/*
+   @exclude      *://*/*?q=*
+   @exclude      *://*/*?s=*
+   @exclude      *://*/*?search=*
+   @grant        none
+
 // @match        *://*/*
 // @exclude        *://google.cn/*
 // @grant        none
@@ -15,11 +21,11 @@
     // You can add or remove keywords as you wish
     // Each category has a different color
     var keywords = {
-        "category1": ["a", "able", "about", "accept", "accord", "according to", "account", "across", "act", "action", "actually", "after", "again", "against", "age", "ago", "agree", "aid", "air", "all", "allow", "almost", "along", "already", "also", "although", "always", "among", "and", "announce", "another", "answer", "any", "anything", "appear", "area", "arm", "army", "around", "art", "as", "ask", "at", "attack", "attempt", "authority", "available", "away", "back", "bad"], // red
-        "category2": ["district", "tear", "balance", "cash", "gay", "damage", "entry", "long-term", "myself", "particular", "aim", "nature", "accuse", "plus", "ice", "teacher", "declare", "hurt", "crime", "anyone", "bill", "station", "network", "immediately", "stuff", "useful", "fifteenth", "ally", "gold", "Dr", "wonderful", "independence", "address", "novel", "refugee", "minority", "manager", "notice", "attend", "shape", "interview", "domestic", "basis", "excellent", "inside", "structure", "spirit", "female", "sleep", "note"], // yellow
-        "category3": ["register", "somewhere", "stupid", "mystery", "crucial", "construction", "lie", "inning", "mate", "ski", "communication", "technical", "rhythm", "dig", "enable", "steam", "exception", "script", "allied", "formula", "beginning", "destruction", "definitely", "yours", "so-called", "guitar", "exposure", "reward", "raid", "steel", "suicide", "offensive", "merely", "hint", "merchant", "brilliant", "admire", "inspector", "regime", "pool", "colonel", "neat", "basically", "done", "racing", "resignation", "agenda", "embassy", "valley", "laboratory"], // blue
-        "category4": ["sour", "freely", "educated", "engaged", "prohibit", "tan", "dispose", "voyage", "doll", "copper", "obsession", "leaflet", "rubber", "unrest", "pathetic", "unwilling", "obituary", "infant", "rehearsal", "berry", "disastrous", "reconciliation", "spike", "pave", "pudding", "sneak", "customs", "winger", "cupboard", "diminish", "glamorous", "mainstream", "dictionary", "weave", "trapped", "contemplate", "settler", "migrate", "pulse", "oz", "distinguished", "dried", "slot", "municipal", "equality", "stranger", "grim", "suburban", "boast", "umpire"], // green
-        "category5": ["aback", "abandonment", "abate", "abdomen", "abdominal", "abduct", "aberration", "abide", "abiding", "abject", "ablaze", "abnormal", "abnormality", "abolition", "aboriginal", "abort", "abortive", "abound", "abrasive", "abreast", "absentee", "absorbed", "absorbing", "absorption", "abstain", "abstention", "abstinence", "abstraction", "abundance", "abundant", "abusive", "abyss", "AC", "acceleration", "accelerator", "accentuate", "accolade", "accompaniment", "accomplice", "accomplishment", "accordance", "accordion", "accountancy", "accredit", "accrue", "accumulation", "accused", "accustom", "accustomed"] // purple
+        "category1": ['accept', 'accord', 'according to', 'account', 'across', 'act', 'action', 'actually', 'after', 'again', 'against', 'age', 'ago', 'agree'], // red
+        "category2": ["two"], // yellow
+        "category3": ["three"], // blue
+        "category4": ["four"], // green
+        "category5": ["five"] // purple
     };
 
     // Define the colors for each category
