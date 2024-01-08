0d7723e6a5fbbf18c5900a463a06e5431ee33174
moodHappy <lhwuhuei@gmail.com>
2024-01-08 09:52:51 +0800

    Delete itools directory

--- itools/myAddEvent.js
+++
@@ -1,7 +0,0 @@
-        function myAddEvent(obj,ev,fn) {
-            if(obj.attachEvent){
-                obj.attachEvent('on'+ev,fn);
-            }else{
-                obj.addEventListener(ev,fn,false);
-            }
-        }
