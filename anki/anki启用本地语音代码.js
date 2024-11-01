13ec6df8e8b62a4a57621b63a965efd328d1fc6a
moodHappy <lhwuhuei@gmail.com>
2024-11-01 17:32:12 +0800

    更新 anki

---
+++ anki/anki启用本地语音代码.js
@@ -0,0 +1,35 @@
+<div class="bottom-container">
+
+
+{{#单词}}
+<div style="display: inline-block;">
+    {{tts zh_CN voices=Apple_Ava:单词}}
+</div>
+{{/单词}}
+
+{{#片假名}}
+<div style="display: inline-block;">
+    {{tts zh_CN voices=Apple_Ava:片假名}}
+</div>
+{{/片假名}}
+
+{{#例句}}
+<div style="display: inline-block;">
+    {{tts zh_CN voices=Apple_Ava:例句}}
+</div>
+{{/例句}}
+
+{{#Source-news}}
+<div style="display: inline-block;">
+    {{tts zh_CN voices=Apple_Ava:Source-news}}
+</div>
+{{/Source-news}}
+
+{{#笔记}}
+<div style="display: inline-block;">
+    {{tts zh_CN voices=Apple_Ava:笔记}}
+</div>
+{{/笔记}}
+
+
+</div>
