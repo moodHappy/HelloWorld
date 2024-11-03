1668aa702ec29f3dd7a8eecea9389118c6e83103
moodHappy <lhwuhuei@gmail.com>
2024-11-03 14:14:48 +0800

    更新 anki

---
+++ anki/anki日文模糊.js
@@ -0,0 +1,42 @@
+日-中正面：
+
+<style>
+.VocabKanji {
+    filter: blur(5px); /* 控制正面的模糊程度 */
+}
+</style>
+
+<div class="VocabKanji">
+    {{FrontSide}}
+    <!-- 正面其他内容 -->
+</div>
+
+日-中背面：
+
+<style>
+.VocabKanji {
+    filter: none; /* 取消背面的模糊效果 */
+}
+</style>
+
+
+中-日正面：
+
+<style>
+.VocabDef {
+    filter: blur(5px); /* 控制正面的模糊程度 */
+}
+</style>
+
+<div class="VocabDef">
+    {{FrontSide}}
+    <!-- 正面其他内容 -->
+</div>
+
+中-日背面：
+
+<style>
+.VocabDef {
+    filter: none; /* 取消背面的模糊效果 */
+}
+</style>
