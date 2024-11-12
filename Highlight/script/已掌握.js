dd7355b45f6fbe1a2a449f9d402012e3136848e1
moodHappy <lhwuhuei@gmail.com>
2024-11-12 19:53:39 +0800

    更新 已掌握.js

--- Highlight/script/已掌握.js
+++ Highlight/script/已掌握.js
@@ -1,2 +1,2 @@
 // 定义已掌握关键词数组
-var masteredKeywords = ['run', 'write', 'study', 'read', 'White', 'watch', 'news', 'new', 'being']; // 已添加 "being"
+var masteredKeywords = ['run', 'write', 'study', 'read', 'White', 'watch', 'news', 'new', 'being', 'anger', 'done']; // 已添加 "anger" 和 "done"
