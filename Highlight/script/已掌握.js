33e5f03223336e745d6b151597f81db1d5a42178
moodHappy <lhwuhuei@gmail.com>
2024-11-15 21:19:39 +0800

    Delete Highlight/script/已掌握.js

--- Highlight/script/已掌握.js
+++
@@ -1,2 +0,0 @@
-// 定义已掌握关键词数组
-var masteredKeywords = ['run', 'write', 'study', 'read', 'White', 'watch', 'news', 'new', 'being', 'anger', 'done', 'found', 'police', 'live']; // 已添加 "police" 和 "live"
